cda60cd4a3775dc6c127c7b5c43935b2570b7d44
Chris Rayner <crayner@spartaglobal.com>
2024-09-20 18:42:19 +0100

    init tests for UserValidation and tests for ValidateName method

---
+++ BankingAppDotNet/validation/UserValidation.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using BankingAppDotNet.services;
+
+namespace BankingAppDotNet.validation;
+
+public static class UserValidation
+{
+    public static bool ValidateName(string name)
+    {
+        return Regex.IsMatch(name, @"^[a-zA-Z][a-zA-Z-]*$");
+    }
+
+    public static bool ValidateEmail(String email)
+    {
+        return false;
+    }
+
+    public static bool ValidatePassword(String password)
+    {
+        return false;
+    }
+
+    public static bool ValidateDateOfBirth(String dateOfBirth)
+    {
+        return false;
+    }
+
+    public static string formatName(String name)
+    {
+        return name.Trim();
+    }
+}
---
+++ BankingAppDotNetTest/validation/UserValidationTest.cs
@@ -0,0 +1,43 @@
+﻿using BankingAppDotNet.validation;
+namespace BankingAppDotNetTest.validation;
+
+public class UserValidationTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestValidateUserNameReturnsTrueWhenNameIsValid()
+    {
+        bool expected = true;
+        bool actual = UserValidation.ValidateName("Bobby");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestValidateUserNameReturnsTrueWhenNameStartsWithSpace()
+    {
+        bool expected = true;
+        bool actual = UserValidation.ValidateName("  Bobby");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestValidateUserNameReturnsFalseWhenNameIsEmpty()
+    {
+        bool expected = false;
+        bool actual = UserValidation.ValidateName("");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    [Test]
+    public void TestValidateUserNameReturnsFalseWhenAllBlankSpace()
+    {
+        bool expected = false;
+        bool actual = UserValidation.ValidateName("      ");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    
+    
+}
