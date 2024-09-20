45cf158151b648b562b4efd61ca83d4e5ffa4390
Chris Rayner <crayner@spartaglobal.com>
2024-09-20 13:02:27 +0100

    init setup of project

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+
+namespace BankingApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
