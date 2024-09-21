85667716149623750700ba8b107b807aec5cf827
Chris Rayner <crayner@spartaglobal.com>
2024-09-22 00:23:00 +0100

    new formatting for table rows in app for banks

--- BankingAppDotNet/core/AccountCreationScreenLogic.cs
+++ BankingAppDotNet/core/AccountCreationScreenLogic.cs
@@ -23,10 +23,19 @@ public class AccountCreationScreenLogic : ConsoleScreen
         {
             BankDto bank = (BankDto)banks[i];
             string display =
-                $"({i + 1})|{bank.bankName}|{bank.bankCode}|{bank.address1}|{bank.address2}|{bank.address3}|{bank.city}";
+                $"({i + 1})|{bank.bankName}|{bank.bankCode}|{bank.address1}|{bank.address2}|{bank.address3}|{bank.city}"; // needs formatting
             output.Add(display);
         }
         int numberOfPages = Convert.ToInt32(Math.Ceiling((double)banks.Count / 5));
+
+        
+        
+        //todo display 5 banks per page, have option to go to next page (if not last), or back a page (if not first), and to select any of the banks on the page
+        //todo example page: | Selection | Bank Name | Sort Code | Address 1 | Address 2 | Address 3 |   City   |
+        //todo               |    (1)    | Barcwest  | 20-12-25  |    15     | The Road  |   Ville   |  Vilton  |
+        //todo               |    (2)    | Barcwest  | 20-22-25  |    16     | The Road  |   Villp   |  Vilton  |
+        
+        
     }
     
 }
--- BankingAppDotNet/user-interface/UserInterfaceComponents.cs
+++ BankingAppDotNet/user-interface/UserInterfaceComponents.cs
@@ -3,17 +3,38 @@
 public static class UserInterfaceComponents
 {
     
-    public const string TopLine =    "--------------------------------------------------------------------------------------------------------------------------";
-    public const string AppName =    "|                                                CRSG Banking Application                                                |";
-    public const string BottomLine = "|________________________________________________________________________________________________________________________|";
-    public const string MiddleLine = "|                                                                                                                        |";
+    public const string TopLine =    "----------------------------------------------------------------------------------------------------------------------------------------------";
+    public const string AppName =    "|                                                          CRSG Banking Application                                                          |";
+    public const string BottomLine = "|____________________________________________________________________________________________________________________________________________|";
+    public const string MiddleLine = "|                                                                                                                                            |";
     
     public static string GetMessageString(string message)
     {
-        int totalWidth = 120;
+        int totalWidth = 160;
         int padding = (totalWidth - message.Length) / 2;
         
         return $"|{"".PadLeft(padding)}{message}{"".PadRight(totalWidth-padding - message.Length)}|";
+    }
+
+    public static string GetBankTableRowString(string selection, string bankName, string sortCode, string address1, string address2, string address3, string city)
+    {
+
+        int totalWidth = 20;
+        int selectionPadding = (totalWidth - selection.Length) / 2;
+        int bankNamePadding = (totalWidth - bankName.Length) / 2;
+        int sortCodePadding = (totalWidth - sortCode.Length) / 2;
+        int address1Padding = (totalWidth - address1.Length) / 2;
+        int address2Padding = (totalWidth - address2.Length) / 2;
+        int address3Padding = (totalWidth - address3.Length) / 2;
+        int cityPadding = (totalWidth - city.Length) / 2;
+        
+        return $"|{"".PadLeft(selectionPadding)}{selection}{"".PadRight(totalWidth-selectionPadding-selection.Length)}|{"".PadLeft(bankNamePadding)}{bankName}{"".PadRight(totalWidth-bankNamePadding-bankName.Length)}"+
+               $"|{"".PadLeft(sortCodePadding)}{sortCode}{"".PadRight(totalWidth-sortCodePadding-sortCode.Length)}|{"".PadLeft(address1Padding)}{address1}{"".PadRight(totalWidth-address1Padding-address1.Length)}"+
+               $"|{"".PadLeft(address2Padding)}{address2}{"".PadRight(totalWidth-address2Padding-address2.Length)}|{"".PadLeft(address3Padding)}{address3}{"".PadRight(totalWidth-address3Padding-address3.Length)}"+
+               $"|{"".PadLeft(cityPadding)}{city}{"".PadRight(totalWidth-cityPadding-city.Length)}|";
+        
+        
+        
     }
 
 }
